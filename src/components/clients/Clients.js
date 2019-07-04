c50557aac45c0ee3626e69b53839ed0a10734842
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-04 11:46:34 +0100

    structure

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Clients extends Component {
+   
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+Clients.propTypes = {
+
+}
+export default Clients
