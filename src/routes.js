2b84cc1866cf7c33bf2404024846445b1a824b2a
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-07 16:21:54 +0100

    detailsclient

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import { Route,  Switch } from 'react-router-dom';
-import App from './App';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
+import ClientDetails from './components/clients/ClientDetails';
 class Routes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path="/App" component={App} />
+          <Route exact path="/" component={Dashboard} />
           <Route
             exact
             path="/layout/Dashboard"
             component={Dashboard}
           />
           <Route exact path="/clients/AddClient"component={AddClient}/>
+        <Route exact path='/client/:id' component={ClientDetails}/>
         </Switch>
       </div>
     );
