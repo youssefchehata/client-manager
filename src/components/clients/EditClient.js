910f6a81aaf802a923934863660f450a91c59851
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-07 21:37:59 +0100

    edtclient

--- src/components/clients/EditClient.js
+++ src/components/clients/EditClient.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// import { compose } from 'redux';
-// import { connect } from 'react-redux';
-// import { firestoreConnect } from 'react-redux-firebase';
-// import Spinner from '../layout/Spinner';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import Spinner from '../layout/Spinner';
 class EditClient extends Component {
     state = {}
     render() { 
@@ -12,4 +12,11 @@ class EditClient extends Component {
     }
 }
  
-export default EditClient;
+export default compose(
+    firestoreConnect(props => [
+      { collection: 'clients', storeAs: 'client', doc: props.match.params.id }
+    ]),
+    connect(({ firestore: { ordered } }, props) => ({
+      client: ordered.client && ordered.client[0]
+    }))
+  )(EditClient);
