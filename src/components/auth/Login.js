74d407a0c4bc4fa1a2cf377a6b125ce2e846e7d8
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-08 23:24:26 +0100

    login component

---
+++ src/components/auth/Login.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+// import { compose } from 'redux';
+// import { connect } from 'react-redux';
+import { firebaseConnect } from 'react-redux-firebase';
+class Login extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="text-center pb-4 pt-3">
+                <span className="text-primary">
+                  <i className="fas fa-lock" />{' '}
+                  Login
+                </span>
+              </h1>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+Login.propTypes = {
+  firebase: PropTypes.object.isRequired
+};
+export default firebaseConnect()(Login);
--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import { Route,  Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
+import Login from './components/auth/Login';
+
 class Routes extends Component {
   render() {
     return (
       <div>
         <Switch>
           <Route exact path="/" component={Dashboard} />
-          <Route
-            exact
-            path="/layout/Dashboard"
-            component={Dashboard}
-          />
-          <Route exact path="/clients/AddClient"component={AddClient}/>
-        <Route exact path='/client/:id' component={ClientDetails}/>
-        <Route exact path='/client/edit/:id' component={EditClient}/>
+          <Route exact path="/layout/Dashboard" component={Dashboard} />
+          <Route exact path="/clients/AddClient" component={AddClient} />
+          <Route exact path="/client/:id" component={ClientDetails} />
+          <Route exact path="/client/edit/:id" component={EditClient} />
+          <Route exact path="/auth/login" component={Login}/>
         </Switch>
       </div>
     );
