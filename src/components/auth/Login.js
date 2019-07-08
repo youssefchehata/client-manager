994356a548dabcec4a33c86179b747ad695eb8fb
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-09 00:24:40 +0100

    ok

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -5,20 +5,61 @@ import PropTypes from 'prop-types';
 // import { connect } from 'react-redux';
 import { firebaseConnect } from 'react-redux-firebase';
 class Login extends Component {
-  state = {};
+  state = {
+    email: '',
+    password: ''
+  };
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  onSubmit = e => {
+    e.preventDefault();
+  };
   render() {
     return (
-      <div className="row">
-        <div className="col-md-6 mx-auto">
-          <div className="card">
-            <div className="card-body">
-              <h1 className="text-center pb-4 pt-3">
-                <span className="text-primary">
-                  <i className="fas fa-lock" />{' '}
-                  Login
-                </span>
-              </h1>
-            
+      <div>
+        <div>
+          <div className="row">
+            <div className="col-md-6 mx-auto">
+              <div className="card">
+                <div className="card-body">
+                  <h1 className="text-center pb-4 pt-3">
+                    <span className="text-primary">
+                      <i className="fas fa-lock" /> Login
+                    </span>
+                  </h1>
+                  <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                      <label htmlFor="email">Email</label>
+                      <input
+                        type="text"
+                        name="email"
+                        required
+                        value={this.state.email}
+                        className="form-control"
+                        onChange={this.onChange}
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="password">Password</label>
+                      <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.onChange}
+                        className="form-control"
+                      />
+                    </div>
+                    <input
+                      type="submit"
+                      value="Login"
+                      className="btn btn-primary btn-block"
+                    />
+                  </form>
+                </div>
+              </div>
             </div>
           </div>
         </div>
