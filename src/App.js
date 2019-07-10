a37201846f278207dc0a8af99f2abf62772f3bd7
jsf.ch6@gmail.com <jsf.ch6@gmail.com>
2019-07-10 23:13:05 +0100

    protection route login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import './App.css';
 
 import AppNavbar from './components/layout/AppNavbar';
--- src/routes.js
+++ src/routes.js
@@ -5,18 +5,18 @@ import AddClient from './components/clients/AddClient';
 import ClientDetails from './components/clients/ClientDetails';
 import EditClient from './components/clients/EditClient';
 import Login from './components/auth/Login';
-
+import {UserIsAuthenticated,UserIsNotAuthenticated} from './helpers/auth'
 class Routes extends Component {
   render() {
     return (
       <div>
         <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/layout/Dashboard" component={Dashboard} />
-          <Route exact path="/clients/AddClient" component={AddClient} />
+          <Route exact path="/" component= {UserIsAuthenticated(Dashboard)} />
+          <Route exact path="/layout/Dashboard" component={UserIsAuthenticated(Dashboard)} />
+          <Route exact path="/clients/AddClient" component={UserIsAuthenticated(AddClient) } />
           <Route exact path="/client/:id" component={ClientDetails} />
-          <Route exact path="/client/edit/:id" component={EditClient} />
-          <Route exact path="/auth/login" component={Login}/>
+          <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient) } />
+          <Route exact path="/auth/login" component={UserIsNotAuthenticated(Login)}/>
         </Switch>
       </div>
     );
